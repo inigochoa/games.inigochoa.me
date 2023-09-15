ee61e04240826c2101727ea4d26f3f9c10a8f10e
Iñigo Ochoa <contact@inigochoa.me>
2023-09-15 23:29:18 +0200

    feat: add lock-code-challenge game

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sitemap(),
+    tailwind(),
+    robotsTxt(),
+  ],
+  site: 'https://games.inigochoa.me'
+});
